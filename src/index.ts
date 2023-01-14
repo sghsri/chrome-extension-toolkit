b8c3cfc81645912ebcd9910c8eb73da408e4e135
Sriram Hariharan <sghsri@gmail.com>
2023-01-14 15:35:37 -0600

    fixed type error

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,16 @@ export * from './messaging';
 
 export const myPackage = (taco = ''): string => `${taco} from my package`;
 
-
-
 interface Test extends MessageDefinition {
-    openNewTab: (data: { url: URL; }) => (URL | undefined)[];
+    openNewTab: (data: { url: URL }) => (URL | undefined)[];
 }
-
-
 
 const m = createBackgroundMessenger<Test>();
 
 let x = await m.openNewTab({ url: new URL('https://google.com') });
 
-
 const handler: MessageHandler<Test> = {
     openNewTab({ data, sendResponse, sender }) {
-        sendResponse();
+        sendResponse([])
     },
 };
