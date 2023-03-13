a5ba315ae53ac2c636dfb82ecfd988a975474a97
Sriram Hariharan <sghsri@gmail.com>
2023-03-13 16:26:18 -0500

    correctly exporting type

--- src/storage/debugStore.ts
+++ src/storage/debugStore.ts
@@ -6,7 +6,7 @@ export const KEY_TO_STORE_MAP = new Map<string, Store>();
  * a helper function to debug the store in the console. Will only do anything when NODE_ENV is set to "development"
  * @param stores an object with the store name as the key and the store as the value
  */
-export function debugStore(stores: { [name: string]: Store }) {
+export function debugStore(stores: { [name: string]: Store<any> }) {
     if (process.env.NODE_ENV === 'development') {
         const names = Object.keys(stores);
         for (const name of names) {
